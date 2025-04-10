f89cc2d0ceebdd10517e3b4681a2ef69403379cb
Lopukhov <lopukhov.evgeniy.o@gmail.com>
2025-04-10 15:48:02 +0500

    init: init repo

---
+++ backend/app/main.py
@@ -0,0 +1,17 @@
+"""Main entry point for the FastAPI application."""
+
+
+def print_hi(name):
+    """
+    Docstring.
+
+    :param name: str
+    :return: None
+    """
+    print(f"Hi, {name}")
+
+
+if __name__ == "__main__":
+    """Init file for the FastAPI backend package."""
+
+    print_hi("PyCharm")
